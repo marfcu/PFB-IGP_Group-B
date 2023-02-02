435cead579d20918a422539359595676ae1b3ca6
ssxbrinx <S10242425@connect.np.edu.sg>
2023-02-02 15:43:40 +0800

    Profit and Loss (2nd Portion)

--- profit_loss.py
+++ profit_loss.py
@@ -29,3 +29,21 @@ def profitloss_function():
         # append days and profit to empty list
         for value in reader:
             profit.append([value[0], value[4]])
+
+        # loop will continue looping if order variable is lesser than the length of profit list
+        while order + 1 < len(profit):
+            
+            # create two variables to store previous and current day values
+            previous = int(profit[order][1])
+            current = int(profit[order + 1][1])
+
+            # evaluate if previous value is bigger than current value
+            if previous > current:
+                # If condition is met, the deficit will be calculated and appended in the summary report  
+                deficit = previous - current
+                with fp_summary.open(mode="a", encoding="UTF-8", newline="") as file:
+                    file.write(f"[PROFIT DEFICIT] DAY: {profit[order + 1][0]}, AMOUNT: USD{deficit}\n")
+                order += 1 # order will increase by 1 
+
+            else: # If condition is not met, order will increase by 1
+                order += 1
